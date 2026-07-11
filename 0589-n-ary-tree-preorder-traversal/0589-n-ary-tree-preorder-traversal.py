71d484688b98ca6506d69068f17168f1c640e57f
Mahesh <23A91A0514@aec.edu.in>
2026-07-11 18:28:29 +0530

    Time: 55 ms (55.88%), Space: 20.9 MB (59.93%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.py
@@ -0,0 +1,30 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        if root is None:
+            return []
+        
+        stack = [root]
+        output = []
+
+        while stack:
+            temp = stack.pop()
+            
+            output.append(temp.val)
+            
+          
+            stack.extend(temp.children[::-1])
+        
+        return output
+        
