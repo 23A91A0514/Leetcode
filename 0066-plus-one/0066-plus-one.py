64fe59559e91eac9d7e2efcc18ef6a3bb7b5cca9
Mahesh <23A91A0514@aec.edu.in>
2026-07-16 10:42:30 +0530

    Time: 0 ms (100%), Space: 19.3 MB (18.13%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,16 +1,12 @@
-from typing import List
-
 class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
-        n = len(digits)
-
-        # Traverse the digits from end to start
-        for i in range(n - 1, -1, -1):
-            if digits[i] < 9:
-                digits[i] += 1
-                return digits
-            digits[i] = 0
-
-        # If loop completes, it means we had all 9s, e.g. [9, 9, 9] -> [0, 0, 0]
-        # So we need to insert 1 at the beginning
-        return [1] + digits
+    def plusOne(self, digits):
+        l=[]
+        s=0
+        for i in digits:
+            s=s*10+i
+        s=s+1
+        while(s>0):
+            digit=s%10
+            l.append(digit)
+            s//=10
+        return l[::-1]
