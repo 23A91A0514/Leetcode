e2250937192a73539935cab7e85e0d93c4008b71
Mahesh <23A91A0514@aec.edu.in>
2026-07-20 11:12:38 +0530

    Time: 0 ms (100%), Space: 19.3 MB (53.14%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -1,10 +1,11 @@
+from collections import Counter
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        n=len(nums)
-        c=0
-        for i in range(n):
-            for j in range(i+1,n):
-                if nums[i]==nums[j]:
-                    c+=1
-        return c
+        ans=0
+        con=Counter(nums)
+        for key , value in con.items():
+            if value>=2:
+              ans+=(value*(value-1))//2
+        return ans
+        
         
