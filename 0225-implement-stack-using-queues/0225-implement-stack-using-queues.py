1d6bfa313488da6d9c8fcdb7a8596258b39fd5e1
Mahesh <23A91A0514@aec.edu.in>
2026-08-12 20:40:18 +0530

    Time: 0 ms (100%), Space: 19.4 MB (86.44%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -1,25 +1,18 @@
-from collections import deque
-
 class MyStack:
 
     def __init__(self):
-        self.q1 = deque()
-        self.q2 = deque()
+        self.q = deque()
 
     def push(self, x: int) -> None:
-        # Step 1: Add x to q2
-        self.q2.append(x)
-        # Step 2: Move all elements from q1 -> q2
-        while self.q1:
-            self.q2.append(self.q1.popleft())
-        # Step 3: Swap q1 and q2
-        self.q1, self.q2 = self.q2, self.q1
+        self.q.append(x)
+        for _ in range(len(self.q) - 1):
+            self.q.append(self.q.popleft())
 
     def pop(self) -> int:
-        return self.q1.popleft()
-
+        return self.q.popleft()
+        
     def top(self) -> int:
-        return self.q1[0]
+        return self.q[0]
 
     def empty(self) -> bool:
-        return not self.q1
+        return len(self.q) == 0
